32c2da880a4bc106072fc29d37c891c23a9bfc5c
LanVlad <vladislav_lanskih@smartexlab.com>
2018-08-07 11:14:09 +0300

    added link to aploaded file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient, HttpEventType} from '@angular/common/http';
 
 @Component({
@@ -9,7 +9,8 @@ import {HttpClient, HttpEventType} from '@angular/common/http';
 export class AppComponent {
   selectedFile: File = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   onFileSelected(event) {
     console.log(event);
@@ -17,19 +18,24 @@ export class AppComponent {
   }
 
   onUpload() {
-    const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-    console.log('fd: ', fd);
-    console.log('selectedFile: ', this.selectedFile);
-    this.http.post('http://localhost:3000/upload', fd, {
-      reportProgress: true, observe: 'events'
-    } )
-      .subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress) {
-          console.log(event.loaded / event.total);
-        } else if (event.type === HttpEventType.Response) {
-          console.log(event);
-        }
-    });
+    if (this.selectedFile) {
+      const fd = new FormData();
+      fd.append('image', this.selectedFile, this.selectedFile.name);
+      console.log('fd: ', fd);
+      console.log('selectedFile: ', this.selectedFile);
+      this.http.post('http://localhost:3000/upload', fd, {
+        reportProgress: true, observe: 'events'
+      })
+        .subscribe(event => {
+          if (event.type === HttpEventType.UploadProgress) {
+            console.log(event.loaded / event.total);
+          } else if (event.type === HttpEventType.Response) {
+            console.log(event);
+            let link = document.getElementById('link');
+            let url = 'http://localhost:3000/images/' + event.body["file"].filename;
+            link.innerHTML = `<a href=${url} target=url>${url}</a>`;
+          }
+        });
+    }
   }
 }
